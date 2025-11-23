5e2e1487456749c5cb6c594ad6e1492d9971e233
IzuBurgher <129360732+IzuBurgher@users.noreply.github.com>
2025-11-23 13:18:08 +1100

    Add files via upload

---
+++ customer_behavior_postgresCode.sql
@@ -0,0 +1,76 @@
+select * from customer limit 10
+
+-- Q1. What is the total revenue generated from males vs females
+select
+gender,
+sum(purchase_amounts) as revenue
+from customer
+group by gender
+
+--Q2. Which customers used a discount but still spent more than the average purchase amount?
+
+select
+customer_id,
+purchase_amounts
+from customer
+where discount_applied = 'Yes' and purchase_amounts >= (select AVG(purchase_amounts) from customer)
+
+--Q3. which are the top 5 products with the highest average review rating??
+
+select
+item_purchased,
+ROUND(AVG(review_rating::numeric) ,2) as "Average Product Rating"
+from customer
+group by item_purchased
+order by AVG(review_rating) DESC
+limit 5
+
+-- Q4. Compare the average purchase amount between standard and express shipping.
+
+select 
+shipping_type, 
+round(AVG(purchase_amounts), 2) AS average_purchase
+from customer
+where shipping_type IN ('Standard', 'Express')
+group by shipping_type
+
+--Q5 Do subscribed customers spend more? Compare average spend and total revenue between subscribers and non subscribers
+
+select
+subscription_status,
+COUNT(customer_id) as total_customers,
+round(avg(purchase_amounts),2) as avg_spend,
+round(sum(purchase_amounts),2) as total_revenue
+from customer
+group by subscription_status
+order by total_revenue, avg_spend;
+
+--Q6. Which 5 products have the highest percentage of purchases with discounts applied?
+
+select 
+item_purchased as products,
+round(100 * sum(case when discount_applied = 'Yes' then 1 else 0 end)/count(*),2) as discount_rate
+from customer
+group by item_purchased
+order by discount_rate desc
+limit 5
+
+--Q7. Segment customers into New, Returning and royal based on their total number of previous purchases, 
+   		--and show the count of each segment
+with customer_type as(
+select customer_id, 
+	previous_purchases, 
+case 
+	when previous_purchases = 1 then 'New'
+	when previous_purchases between 2 and 10 then 'Returning'
+	else 'Loyal'
+	end as customer_segment
+from customer
+) 
+select 
+customer_segment,
+count(*) as "Number of Customers"
+from customer_type
+group by customer_segment
+
+
